f531c903fc0f3c782dc1be76c5675add26bda300
hy.amanieu <hy.amanieu@leclanche.com>
2023-12-04 13:28:07 +0100

    threading in main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from panel import serve
 from dashboard_a import DashboardA
 from dashboard_b import DashboardB
 from dashboard_c import DashboardC
-
+import panel as pn
 dashboards = {'A': DashboardA().view, 'B': DashboardB().view, 'C': DashboardC().view}
 
 if __name__ == '__main__':
-    serve(dashboards, admin=True, threaded=True, show=False)
+    #pn.extension(nthreads=8)
+    serve(dashboards, admin=True, threaded=False, show=False)
